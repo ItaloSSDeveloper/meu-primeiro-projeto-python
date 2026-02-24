c829427f3ea23866bbd7bf09bcb62a84a4950acb
ItaloSSDeveloper <italosoaresz931@gmail.com>
2026-02-24 12:08:55 -0300

    Primeiro projeto Python

---
+++ app.py
@@ -0,0 +1,11 @@
+# Lista de vendas
+vendas = [150, 200, 300, 100, 50]
+
+# Somar todas as vendas
+total = sum(vendas)
+
+# Calcular média
+media = total / len(vendas)
+
+print("Total de vendas:", total)
+print("Média de vendas:", media)
